4bfaaaafad6160818c4b91631c1936a116040bcc
Davi Roberto <daviroberto11@hotmail.com>
2022-02-12 23:09:12 -0300

    styles testing

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,15 @@
+import * as S from './styles'
+
 const Main = () => (
-  <div>
-    <h1>Boilerplate</h1>
-  </div>
+  <S.Wrapper>
+    <S.Illustration src="img/react-animation.gif" alt="Image ReactJS animated"></S.Illustration>
+
+    <S.Title>Boilerplate</S.Title>
+
+    <S.Description>
+      TypeScript, React, NextJS and Styled Components
+    </S.Description>
+  </S.Wrapper>
 )
 
 export default Main
